0dc8c2c5007b7c990de82c05df0f937f82e07a01
haotianteng <havens.teng@gmail.com>
2018-06-28 03:30:21 +1000

    concat test and train dataset before preprocess, regard image_top_1 as a categorical feature

--- gbdt.py
+++ gbdt.py
@@ -10,19 +10,32 @@ import numpy as np
 import csv
 from gbdt_input import prepare_train_dataset as data
 from gbdt_input import balance_datasets
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
 
-CAT_LABELS = ['region','city','parent_category_name','category_name','param_1','param_2','param_3','user_type']
-VAL_LABELS = ['price','image_top_1']
+CAT_LABELS = ['region','city','image_top_1','parent_category_name','category_name','param_1','param_2','param_3','user_type']
+VAL_LABELS = ['price']
 
 print('load data.')
 train_f = '/home/heavens/cqt-avito/data/train.csv'
 test_f = '/home/heavens/cqt-avito/data/test.csv'
 pred_file = '/home/heavens/cqt-avito/data/test_summary'
 
-x_train,y_train,x_valid,y_valid = data(train_f,valid_size = 1000)
-x_test = data(test_f,for_eval = True)
-datasets_iter = balance_datasets(x_train,y_train)
-
+train_df = pd.read_csv(train_f,sep = ',',header = 0,index_col = "item_id", parse_dates = ["activation_date"])
+test_df = pd.read_csv(test_f,sep = ',',header = 0,index_col = "item_id", parse_dates = ["activation_date"])
+ntrain = train_df.shape[0]
+traindex = train_df.index
+ntest = test_df.shape[0]
+testindex = test_df.index
+y = train_df['deal_probability']
+train_df.drop('deal_probability',axis = 1,inplace = True)
+df = pd.concat([train_df,test_df],axis=0)
+del train_df,test_df
+df = data(df)
+x = df.loc[traindex,:]
+x_test = df.loc[testindex,:]
+x_train, x_valid, y_train, y_valid = train_test_split(
+        x, y, test_size=0.10, random_state=2018)
 print('Train data size:' + str(x_train.size))
 print('Valid data size:' + str(x_valid.size))
 
@@ -44,17 +57,17 @@ params = {
 #    break
 lgb_train = lgb.Dataset(x_train, 
                         label = y_train,
-                       free_raw_data=False)
-lgb_valid = lgb.Dataset(x_valid, label = y_valid, free_raw_data = False)
+                        categorical_feature = CAT_LABELS)
+lgb_valid = lgb.Dataset(x_valid, label = y_valid,categorical_feature = CAT_LABELS)
 
 print('begin training')
 gbm = lgb.train(params,
                 lgb_train,
                 num_boost_round=20000,
-                valid_sets=lgb_valid,
+                valid_sets=[lgb_train,lgb_valid],
                 feature_name = x_train.columns.tolist(),
-                categorical_feature = CAT_LABELS,
-                early_stopping_rounds = 50)
+                early_stopping_rounds = 50,
+                verbose_eval=10)
 
 print('Save model...')
 # save model to file
@@ -62,8 +75,8 @@ gbm.save_model('model.txt')
 
 print('Start predicting...')
 # predict
-y_pred = gbm.predict(x_test, num_iteration=gbm.best_iteration)
-
+y_pred = gbm.predict(x_test)
+print('RMSE:', np.sqrt(mean_squared_error(y_valid, gbm.predict(x_valid))))
 with open(pred_file,'w+') as f:
     f.write('item_id,deal_probability')
     f.write('\n')
@@ -72,7 +85,7 @@ with open(pred_file,'w+') as f:
             y_pred[idx] = 0
         if y_pred[idx]>1:
             y_pred[idx] =1
-        f.write(str(x_test['item_id'][idx]))
+        f.write(x_test.index[idx])
         f.write(',')
         f.write(str(y_pred[idx]))
         f.write('\n')
--- gbdt_input.py
+++ gbdt_input.py
@@ -31,33 +31,15 @@ from scipy.sparse import hstack, csr_matrix
 from nltk.corpus import stopwords 
 
 
-CAT_LABELS = ['user_id','region','city','parent_category_name','category_name','param_1','param_2','param_3','user_type']
-VAL_LABELS = ['price','image_top_1']
-DROP_LABELS = ['deal_probability','activation_date','image','item_seq_number']
-
-def prepare_train_dataset(csv_file,valid_size = None,for_eval = False):
-    df = pd.read_csv(csv_file,sep = ',',header = 0,index_col = "item_id", parse_dates = ["activation_date"])
+CAT_LABELS = ['user_id','image_top_1','region','city','parent_category_name','category_name','param_1','param_2','param_3','user_type']
+VAL_LABELS = ['price']
+DROP_LABELS = ['title','description','activation_date','image','item_seq_number']
+RANDOM_SEED = 2018
+def prepare_train_dataset(df):
     df = preprocess(df)
-    
-    if not for_eval:
-        y = df['deal_probability'].values
-        x = df.drop(labels = DROP_LABELS, axis = 1)
-    else:
-        x = df.drop(labels = DROP_LABELS[1:],axis = 1)
-    nan_x = x == 'NaN'
+    x = df.drop(labels = DROP_LABELS, axis = 1)
     x = retyping(x,cat_labels = CAT_LABELS, val_labels = VAL_LABELS)
-    x[nan_x] = np.nan
-    if for_eval:
-        return x
-    else:
-        if valid_size is None:
-            return(x,y)
-        else:
-            x_valid = x[:valid_size]
-            y_valid = y[:valid_size]
-            x_train = x[valid_size:]
-            y_train = y[valid_size:]
-            return (x_train,y_train,x_valid,y_valid)
+    return(x)
 def cleanName(text):
     try:
         textProc = text.lower()
@@ -100,51 +82,6 @@ def preprocess(df):
     # Extra Feature Engineering
     df['title_desc_len_ratio'] = df['title_num_letters']/df['description_num_letters']
     
-#    print("\n[TF-IDF] Term Frequency Inverse Document Frequency Stage")
-#    russian_stop = set(stopwords.words('russian'))
-#    
-#    tfidf_para = {
-#        "stop_words": russian_stop,
-#        "analyzer": 'word',
-#        "token_pattern": r'\w{1,}',
-#        "sublinear_tf": True,
-#        "dtype": np.float32,
-#        "norm": 'l2',
-#        #"min_df":5,
-#        #"max_df":.9,
-#        "smooth_idf":False
-#    }
-#    
-#    
-#    def get_col(col_name): return lambda x: x[col_name]
-#    ##I added to the max_features of the description. It did not change my score much but it may be worth investigating
-#    vectorizer = FeatureUnion([
-#            ('description',TfidfVectorizer(
-#                ngram_range=(1, 2),
-#                max_features=17000,
-#                **tfidf_para,
-#                preprocessor=get_col('description'))),
-#            ('title',CountVectorizer(
-#                ngram_range=(1, 2),
-#                stop_words = russian_stop,
-#                #max_features=7000,
-#                preprocessor=get_col('title')))
-#        ])
-#        
-#    start_vect=time.time()
-#    
-#    #Fit my vectorizer on the entire dataset instead of the training rows
-#    #Score improved by .0001
-#    vectorizer.fit(df.to_dict('records'))
-#    
-#    ready_df = vectorizer.transform(df.to_dict('records'))
-#    tfvocab = vectorizer.get_feature_names()
-#    print("Vectorization Runtime: %0.2f Minutes"%((time.time() - start_vect)/60))
-    
-    # Drop Text Cols
-    textfeats = ["description", "title"]
-    df.drop(textfeats, axis=1,inplace=True)
-#    tfvocab = df.columns.tolist() + tfvocab
     return df
 
 def balance_datasets(x,y,amp = None, shuffle = True):
